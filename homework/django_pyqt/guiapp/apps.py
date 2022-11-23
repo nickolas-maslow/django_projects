efc58e614e011994906fdf176cc6a038d4991de5
nickolas-maslow <99341072+nickolas-maslow@users.noreply.github.com>
2022-11-23 20:49:17 +0600

    Delete homework/django_pyqt/guiapp directory

--- homework/django_pyqt/guiapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GuiappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'guiapp'
--- homework/django_pyqt/guiapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Note(models.Model):
-    user = models.CharField(max_length=128, blank=False, unique=True)
-
-    def __str__(self):
-        return self.user
-
