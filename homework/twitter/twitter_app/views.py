44e60e7fc94cd13a53626deb2c2c3129004f00fc
nickolas-maslow <99341072+nickolas-maslow@users.noreply.github.com>
2022-11-27 19:01:26 +0600

    add pagination, searching

--- homework/twitter/twitter_app/views.py
+++ homework/twitter/twitter_app/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
 from .services import add_user
+from django.views.generic import ListView
+from twitter_app.models import User
+from django.core.paginator import Paginator
+
+
+def listing(request):
+    contact_list = User.objects.all()
+    paginator = Paginator(contact_list, 25) 
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'index.html', {'page_obj': page_obj})
+class UserListView(ListView):
+    paginate_by = 2
+    model = User
+
+class ESearchView(View):
+    template\_name = 'search/index.html'
+ 
+    def get(self, request, *args, **kwargs):
+        context = {}
+ 
+        question = request.GET.get('q')
+        if question is not None:
+            search\_articles = Article.objects.filter(article\_content\_\_search=question)
+            context['last\_question'] = '?q=%s' % question
+ 
+            current\_page = Paginator(search\_articles, 10)
+ 
+            page = request.GET.get('page')
+            try:
+                context['article\_lists'] = current\_page.page(page)
+            except PageNotAnInteger:
+                context['article\_lists'] = current\_page.page(1)
+            except EmptyPage:
+                context['article\_lists'] = current\_page.page(current\_page.num\_pages)
+ 
+        return render\_to\_response(template\_name=self.template\_name, context=context)
 
 
 def index(request):
